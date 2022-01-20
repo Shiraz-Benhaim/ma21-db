780cb0fc8c4efde8ef07098cbff4e4c7d6d0d707
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-20 11:31:07 +0200

    Change the type of the items to LinkedHashMap to save the order

--- src/main/java/com/database/db/modules/Record.java
+++ src/main/java/com/database/db/modules/Record.java
@@ -3,16 +3,15 @@ package com.database.db.modules;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
 
 @Data
 @AllArgsConstructor
 public class Record {
-    private Map<String ,Item> items;
+    private LinkedHashMap<String, Item> items;
 
     public Record() {
-        this.items = new HashMap<>();
+        this.items = new LinkedHashMap<>();
     }
 
     public void addItem(String colName, Item item) {
