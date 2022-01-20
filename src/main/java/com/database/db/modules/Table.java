b82698c87acda57340500838491b86a19a6121d7
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-20 11:32:43 +0200

    class Table: Update the toString function

--- src/main/java/com/database/db/modules/Table.java
+++ src/main/java/com/database/db/modules/Table.java
@@ -1,38 +1,48 @@
 package com.database.db.modules;
 
-import lombok.AllArgsConstructor;
+import com.database.db.utils.ColumnSearch;
+import com.database.db.validation.RecordValidation;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
-@AllArgsConstructor
 public class Table {
+    private List<Column> columns;
     private List<Record> records;
 
-    public Table() {
+    public Table(List<Column> columns) {
+        this.columns = columns;
         this.records = new ArrayList<>();
     }
 
     public void addRecord(Record record) {
-        this.records.add(record);
+        RecordValidation recordValidation = new RecordValidation();
+        if (recordValidation.isValid(record, this.columns)) {
+            this.records.add(record);
+        }
     }
 
     @Override
     public String toString() {
         String data = "";
-        ArrayList<String> headers = new ArrayList<>();
 
-        for (String header : this.records.get(0).getItems().keySet()) {
-            data += String.format("|%10s|", header);
-            headers.add(header);
+        // Headers
+        for (Column col : this.columns) {
+            data += String.format("%10s|", col.getName());
         }
 
+        // Records
         for (Record record : this.records) {
             data += "\n";
-            for (String header : headers) {
-                data += String.format("|%10s|", record.getItems().get(header).getValue());
+            for (String header : this.columns.stream()
+                                    .map(Column::getName)
+                                    .collect(Collectors.toList())) {
+                data += String.format("%10s|", record.getItems().get(header).getValue());
             }
         }
 
